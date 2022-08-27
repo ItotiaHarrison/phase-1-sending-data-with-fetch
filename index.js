e443dd0aaaeb3ef686319c0b0807a92a28fe8cb7
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-08-28 00:30:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+     method: "POST",
+     headers: {
+         "Content-Type": "application/json",
+         Accept: "application/json",
+     },
+     body: JSON.stringify({
+         name: name, 
+         email: email,
+         })
+    })
+    .then(res => res.json())
+    .then(user => console.log(user))
+
+ .catch(error => {
+     let err = document.createElement("p");
+     err.textContent = error.message;
+     document.body.appendChild(er);
+ });
+ 
+ }
+ submitData('harry', "harry@gmail.com")
